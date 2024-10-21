79b06946ee47132837384a0bbb480e5419a6d5eb
eliasailenei <82527761+eliasailenei@users.noreply.github.com>
2024-10-21 17:51:35 +0100

    added new feature, added xml support

--- cli.py
+++ cli.py
@@ -1,1 +1,12 @@
 print("cli")
+import lib
+
+lin1 = lib.Lineage()
+
+
+exporter = lib.ImExPorter()
+lineages = exporter.importProgress("family_tree_2024-10-21_17-31-27.xml")
+
+for i in lineages:
+    print(i)
+# Call saveProgress using the instance
--- lib.py
+++ lib.py
@@ -1,4 +1,6 @@
+import time, datetime
 import xml.etree.ElementTree as ET
+import xml.dom.minidom
 class Lineage:
     def __init__(self):
         self.persons = []
@@ -10,7 +12,7 @@ class Lineage:
     def _add_person(self, person):
         self.persons.append(person)
 
-    def create_new_person(self):
+    def create_new_person_cli(self):
         """This code creates a new Person vairable and stores them into the Lineage list."""
         qList = ["Please enter the name:", "Please enter the DOB:", "Are they alive?:", "What is their ethnicity?:"]
         userIn = [len(qList)]
@@ -29,6 +31,46 @@ class Person:
         self.ethnicity = ethnicity
 
 class ImExPorter:
-    def saveProgress(lineages):
+    def saveProgress(self, lineages):
+        ts = time.time()
         rootNode = ET.Element("FamilyTree")
+        for i, lineage in enumerate(lineages): 
+            lineage = ET.SubElement(rootNode, "Lineage", id=str(i)) 
+            person_id = 1  
+            for person in lineage.persons:
+                person_element = ET.SubElement(lineage, "Person", id=str(person_id))  
+                ET.SubElement(person_element, "Name").text = str(person.name)
+                ET.SubElement(person_element, "DOB").text = str(person.dob)
+                ET.SubElement(person_element, "AliveStatus").text = str(person.is_alive)  
+                ET.SubElement(person_element, "Ethnicity").text = str(person.ethnicity)
+                person_id += 1 
+        filename = "family_tree_" + datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H-%M-%S') + ".xml"
+        tree = ET.ElementTree(rootNode)
+        rough_string = ET.tostring(rootNode, encoding="utf-8")
+        reparsed = xml.dom.minidom.parseString(rough_string)
+        pretty_xml = reparsed.toprettyxml(indent="  ")
+        with open(filename, "w", encoding="utf-8") as file:
+            file.write(pretty_xml)
+
+    def importProgress(self,file_path):
+        tree = ET.parse(file_path)
+        root = tree.getroot()
+        lineages = []
+        for lineage_element in root.findall('Lineage'):
+            lineage_id = lineage_element.get('id')
+            lineage = Lineage()
+            for person_element in lineage_element.findall('Person'):
+                person = Person(
+                    name=person_element.find('Name').text,
+                    dob=person_element.find('DOB').text,
+                    is_alive=person_element.find('AliveStatus').text,
+                    ethnicity=person_element.find('Ethnicity').text
+                )
+                lineage._add_person(person)
+            lineages.append(lineage)
+
+        return lineages
+
+
+
 
