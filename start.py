0e50492eeddfe8c504745e52999e7d5e4b14b0b3
anthonybell1211@gmail.com <anthonybell1211@gmail.com>
2024-11-21 09:57:46 +0000

    import not working

--- start.py
+++ start.py
@@ -1,119 +1,189 @@
-import clear
 import os
+import yaml
 from main import FamilyTree
+from family_lib import Parent, Child, Partner, ParentChild
 
 
-class main_prog:
+class StartProgram:
     def __init__(self):
-        self.save_files = []
+        self.saves = []  # Empty list for saved files
 
-    def chk_lib(self):
-        missing = []
-        try:
-            import curses
-        except ImportError:
-            missing.append("windows-curses")
-        try:
-            import yaml
-        except ImportError:
-            missing.append("pyyaml")
-        if not missing:
-            print("Libraries already present...")
-            clear.clear()
-            return True
+    def load_family_data(self, file_name):
+        """Load the family data from the specified YAML file."""
+        
+        # Define the path to the 'Saves' folder
+        save_folder = "C:/Users/ASUS/OneDrive - University of Greenwich/Computer Science Modules/reposo/Saves"
+        
+        # Construct the full path to the YAML file
+        full_file_path = os.path.join(save_folder, file_name)
+
+        if os.path.exists(full_file_path):
+            with open(full_file_path, 'r') as file:
+                return yaml.safe_load(file)  # Load the family data from the file
         else:
-            for lib in missing:
-                print(
-                f"{lib} library is missing. Please install it in order for this program to work! "
-                "Do this --> \n\n"
-                "1) Make sure you are running the latest version of Python.\n"
-                "2) Open a new privileged CMD and type:\n"
-                f"   pip install {lib}\n"
-                "3) Re-open this program.\n\n"
-                )
-        return False
+            print("Error: No file loaded")
+            create_new = input("Would you like to create a new save? (y/n): ")
+            if create_new.lower() == 'y':
+                # Create a new save file if the user agrees
+                self.create_new_save()
+            return {"members": []}  # Return an empty family structure if file doesn't exist
+
+    def save_family_data(self, file_name, data):
+        """Save the family data to the specified YAML file."""
+        with open(file_name, 'w') as file:
+            yaml.dump(data, file)
+
+    def mainMenu(self, save_selection):
+     """Main menu where the user can choose between CLI and GUI modes."""
+     if save_selection == -1:
+        create_new_save = input("No saved family tree files found. Would you like to create a new save? (y/n): ")
+        if create_new_save.lower() == "y":
+            self.create_new_save()
+            save_selection = 0  # After creating a new save, we set this to 0 to proceed
+        else:
+            print("Exiting program as no save is selected.")
+            exit()
+
+     # Check if save_selection is within bounds before accessing the saves list
+     if save_selection < 0 or save_selection >= len(self.saves):
+        print("Invalid save selection.")
+        return  # Exit the method if the selection is invalid
+
+     file_name = self.saves[save_selection]  # Use the self.saves list to get the file
+     family_data = self.load_family_data(file_name)
+
+     # Ensure family_data is a dictionary and contains "members"
+     if isinstance(family_data, dict) and "members" in family_data:
+        family_members = []
+        if family_data["members"]:
+            for member_data in family_data["members"]:  # Iterating over the "members" key
+                role = member_data.pop("role", "Parent")  # Default role is "Parent"
+                if role == "Parent":
+                    person = Parent(**member_data)
+                elif role == "Child":
+                    person = Child(**member_data)
+                elif role == "Partner":
+                    person = Partner(**member_data)
+                elif role == "ParentChild":
+                    person = ParentChild(**member_data)
+                else:
+                    raise ValueError(f"Unknown role: {role}")
+                family_members.append(person)
+        else:
+            print("Warning: No family members found in this save.")
+            # Optionally, allow the user to add members or proceed with empty data
+            add_members = input("Would you like to add family members? (y/n): ")
+            if add_members.lower() == 'y':
+                # You can implement the functionality to add new members here
+                pass
+     else:
+        print("Invalid family data structure. Exiting...")
+        print(family_data)  # Debugging to inspect the loaded family data
+
+        return  # Exit the method if the structure is not as expected
+
+     # Menu for CLI vs GUI selection
+     finish = False
+     while not finish:
+        print("=" * 45)
+        print("Welcome to the Family Tree Program!")
+        print("Please select the version you wish to run:")
+        print("1) Command Line Interface Mode")
+        print("2) Graphical User Interface Mode")
+        print("3) Exit program")
+        print("=" * 45)
+
+        try:
+            user_option = int(input("Your option: "))
+            if user_option == 1:
+                # Running the CLI Mode
+                program = FamilyTree(self.saves)  # Pass self.saves instead of saves
+                program.main()
+                finish = True
+            elif user_option == 2:
+                # Running the GUI Mode
+                print("Launching the GUI...")
+                gui_program = FamilyTreeGUI(family_members)
+                gui_program.display_family()  # This should launch your Tkinter GUI window
+                finish = True
+            elif user_option == 3:
+                finish = True
+                exit()
+            else:
+                print("Sorry, I didn't understand the input.")
+        except ValueError:
+            print("Invalid input. Please enter a numerical value.")
+
+
+
+    def create_new_save(self):
+     """Create a new family save file with an empty family structure."""
+     # Implement save creation logic, e.g., ask for a file name and save a blank family structure
+     print("Creating a new save...")
+     new_file_name = input("Enter the name for the new save file (without .yaml): ") + ".yaml"
+     save_folder = "C:/Users/ASUS/OneDrive - University of Greenwich/Computer Science Modules/reposo/Saves"
+     full_file_path = os.path.join(save_folder, new_file_name)
+
+     # Ensure the new save file contains the correct structure
+     data = {"members": []}  # Start with an empty family structure
+     with open(full_file_path, 'w') as new_file:
+        yaml.dump(data, new_file)  # Save the empty structure to the file
+
+     print(f"New family tree save created: {full_file_path}")
+
+
+    def main(self):
+        """Main entry point for the program."""
+        print("There is a bug with PyCharm and VS22 where the clear command does not work." 
+              " We recommend you DO NOT run this program on the debug console but instead with cmd.exe")
+        print("Press enter to dismiss...")
+        input()
+
+        # Get the current directory and search for family tree files
+        cur_dir = os.path.join(os.getcwd(), "Saves")  # Make sure it points to the Saves folder
+        print(f"Looking for files in: {cur_dir}")  # Debugging print
+        save_selection = -1  # Set to -1 if no files are found
+        
+        # List all files in the Saves folder and check
+        for file_name in os.listdir(cur_dir):
+            print(f"Found file: {file_name}")  # Debugging print to see files in Saves folder
+            if file_name.endswith(".yaml"):  # Ensure it ends with .yaml
+                self.saves.append(file_name)
+        
+        if len(self.saves) > 0:
+            save_selection = self.loadSaves(self.saves)
+        else:
+            print("No saved family tree files found.")
+            create_new = input("Would you like to create a new save? (y/n): ")
+            if create_new.lower() == 'y':
+                # Create a new save file
+                self.create_new_save()
+
+        # Show the main menu for CLI/GUI selection
+        self.mainMenu(save_selection)
 
     def loadSaves(self, saves):
+        """Load the saved family tree files."""
         is_done = False
         while not is_done:
-            clear.clear()
             print("=" * 50)
-            print()
             print("We found your previous saves, please select one:")
-            print()
             print(" 0: Do not load a save")
             for i in range(len(saves)):
                 print(f" {i + 1}: {saves[i].replace('family_tree_', '')}")
-            print()
             print("=" * 50)
 
             option = input("Enter your choice: ")
 
             if option.isdigit() and 0 <= int(option) <= len(saves):
-                option = int(option)
+                option = int(option) - 1  # Adjust option to match list index (starts from 0)
                 is_done = True
             else:
                 print("Sorry, I didn't understand the input.")
 
         return option
 
-    def mainMenu(self, selection):
-        finish = False
-        saves = "na"
-        if selection > 0:
-            saves = self.save_files[selection - 1]
-        while not finish:
-            clear.clear()
-            print("=" * 45)
-            print()
-            print("Welcome to the Family Tree Program!")
-            print()
-            print("https://github.com/Anth0nyB311/familytree")
-            print()
-            print("Please select the version you wish to run:")
-            print("1) Command Line Interface Mode")
-            print("2) Graphical User Interface Mode")
-            print("3) Exit program")
-            print()
-            print("=" * 45)
-            try:
-                user_option = int(input("Your option: "))
-                if user_option == 1:
-                    program = FamilyTree(saves)
-                    program.main()
-                    finish = True
-                elif user_option == 2:
-                    # Anthony, place your code here
-                    finish = True
-                elif user_option == 3:
-                    finish = True
-                    exit()
-                else:
-                    print("Sorry, I didn't understand the input.")
-            except ValueError:
-                print("Invalid input. Please enter a numerical value.")
-    def main(self):
-        print("There is a bug with PyCharm and VS22 where the clear command does not work." 
-        " We recommend you DO NOT run this program on the debug console but instead with cmd.exe"
-        )
-        print()
-        print("Press enter to dismiss...")
-        input()
-        clear.clear()
-        if not self.chk_lib():
-            exit()
-        cur_dir = os.getcwd()
-        save_selection = 0
-        for file_name in os.listdir(cur_dir):
-            if file_name.startswith(
-                    "family_tree_") and file_name.endswith(".yaml"):
-                self.save_files.append(file_name)
-        if len(self.save_files) > 0:
-            save_selection = self.loadSaves(self.save_files)
-        clear.clear()
-        self.mainMenu(save_selection)
-
 
 if __name__ == "__main__":
-    prog = main_prog()
+    prog = StartProgram()
     prog.main()
--- yaml_lib.py
+++ yaml_lib.py
@@ -51,18 +51,25 @@ def yaml_export(family, filename=return_save_filename()):
 
 def yaml_import(filename):
     """imports the yaml"""
+    print("Importing file...")
     try:
         with open(filename, "r") as f:
             family_data = yaml.safe_load(f)
 
         if not family_data:
-            print(f"File not found, starting with empty family.")
+            print(f"File not found or empty, starting with empty family.")
             return []
+
         id_to_person = {}
         family = []
 
         for person_dict in family_data:
-            person_type = person_dict.get("type", "Person")
+            person_type = person_dict.get("type", "Person")  # Default to "Person" if type is missing
+
+            # Remove id from person_dict before passing to constructor
+            person_dict.pop("id", None)
+
+            # Initialize the person object based on type
             if person_type == "Parent":
                 person = Parent(
                     name=person_dict["name"],
@@ -93,38 +100,41 @@ def yaml_import(filename):
                 )
             else:
                 raise ValueError(f"Unknown person type: {person_type}")
-            person.id = person_dict["id"]
-            if not person.is_alive:
-                person.death_date = person_dict.get("death_date")
-            else:
-                person.death_date = None
+
+            # Set the ID and other attributes (death date, etc.)
+            person.death_date = person_dict.get("death_date", None) if not person.is_alive else None
             id_to_person[person.id] = person
             family.append(person)
-        max_id = max(id_to_person.keys(), default=0)
-        if hasattr(Person, "_id_counter"):
-            Person._id_counter = max_id + 1
-        else:
-            Person._id_counter = max_id + 1
+
+        # Now we restore the relationships (children, parents, partners, siblings)
         for person_dict in family_data:
             person = id_to_person[person_dict["id"]]
+
+            # Add children
             for child_id in person_dict.get("children_ids", []):
                 child = id_to_person.get(child_id)
                 if child and child not in getattr(person, "children", []):
                     person.children.append(child)
                     if person not in getattr(child, "parents", []):
                         child.parents.append(person)
+
+            # Add partners
             for partner_id in person_dict.get("partners_ids", []):
                 partner = id_to_person.get(partner_id)
                 if partner and partner not in getattr(person, "partners", []):
                     person.partners.append(partner)
                     if person not in getattr(partner, "partners", []):
                         partner.partners.append(person)
+
+            # Add parents
             for parent_id in person_dict.get("parents_ids", []):
                 parent = id_to_person.get(parent_id)
                 if parent and parent not in getattr(person, "parents", []):
                     person.parents.append(parent)
                     if person not in getattr(parent, "children", []):
                         parent.children.append(person)
+
+            # Add siblings
             for sibling_id in person_dict.get("siblings_ids", []):
                 sibling = id_to_person.get(sibling_id)
                 if sibling and sibling not in getattr(person, "siblings", []):
@@ -132,8 +142,36 @@ def yaml_import(filename):
                     if person not in getattr(sibling, "siblings", []):
                         sibling.siblings.append(person)
 
-        print(f"{filename} was a success.")
+        print(f"{filename} loaded successfully.")
         return family
+
+    except FileNotFoundError:
+        print(f"File {filename} not found, starting with empty family.")
+        return []
+    except yaml.YAMLError as e:
+        print(f"Error parsing the save file: {e}")
+        return []
+    except ValueError as ve:
+        print(f"Value error occurred: {ve}")
+        return []
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        return []
+
+
+    except FileNotFoundError:
+        print(f"File {filename} not found, starting with empty family.")
+        return []
+    except yaml.YAMLError as e:
+        print(f"Error parsing the save file: {e}")
+        return []
+    except ValueError as ve:
+        print(f"Value error occurred: {ve}")
+        return []
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        return []
+
 
     except FileNotFoundError:
         print(f"File not found, starting with empty family.")
